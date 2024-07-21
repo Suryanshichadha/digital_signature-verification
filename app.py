09b09562ab44a6d3c8b5cf37bb0f029ecbd8bf7a
Suryanshichadha <159747462+Suryanshichadha@users.noreply.github.com>
2024-07-21 17:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ footer_html ="""
         }
     </style>
     <div class="footer">
-        <p>© 2024 Digital Signature Application. All rights reserved.</p>
+        <p>© VIPS Digital Signature Application. All rights reserved </p>
         <p><a href="https://github.com/Suryanshichadha/digital_signature-verification" target="_blank">GitHub Repository</a></p>
     </div>"""
 
-st.title("Digital Signature verification")
-st.image("dig.png")
+st.title("DOCUMENT AUTHENTICATION")
+st.image("dig.png",width=400)
 
 
 page = st.sidebar.selectbox("Choose a function", ["Home", "Sign Document", "Verify Document"])
@@ -46,8 +46,11 @@ st.sidebar.write("STEPS TO BE FOLLOWED")
 st.sidebar.write("Select the function to be performed")
 st.sidebar.write("1. If you want to generate sign for a document choose sign document from drop down and download the sign and hash")
 st.sidebar.write("2. If you want to verify the document upload the documents and click verify ")
+
 if page == "Home":
-    st.write("Welcome to the Digital Signature Application.")
+    st.write("WELCOME TO THE DOCUMENT AUTHENTICATION APPLICATION.")
+    st.write("hello hope you are doing great and your documents too....")
+    st.write("Let me help you with your document verification, i can also give a signature to your document.")
     st.write("This application helps you create a sign for your document and let you verify the integrity of the documents.")
     st.write("A digital signature is a mathematical scheme for verifying the authenticity and integrity of digital messages or documents.")
     st.write("Hash value is used to create a digital fingerprint of the document. Any change in the document, no matter how small, will produce a different hash value.")
@@ -78,3 +81,4 @@ elif page == "Verify Document":
             else:
                 st.error("Document verification failed.")
 st.markdown(footer_html, unsafe_allow_html=True)
+
